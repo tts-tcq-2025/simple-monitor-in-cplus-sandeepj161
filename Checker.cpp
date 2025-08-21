376d2d491039e62b6b8c8d0bc56ec151b3289cc5
Sandeep J <j.sandeep.private@gmail.com>
2025-08-21 08:52:50 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -16,33 +16,19 @@ BreachType getBreachType(float value, float low, float high) {
   return BreachType::NORMAL;
 }
 
-BreachType getChargeRateBreach(float chargeRate) {
-  // To complete coverage, treat <0 as TOO_LOW (abnormal for charging phase)
-  return getBreachType(chargeRate, 0, 0.8);
+void printBreach(const string& param, BreachType breach) {
+  if (breach == BreachType::TOO_LOW) {
+    cout << param << " too low!\n";
+  } else if (breach == BreachType::TOO_HIGH) {
+    cout << param << " too high!\n";
+  }
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
   auto tempBreach = getBreachType(temperature, 0, 45);
   auto socBreach = getBreachType(soc, 20, 80);
-  auto chargeRateBreach = getChargeRateBreach(chargeRate);
-
-  // Print breaches (I/O)
-  auto printBreach = [](const string& param, BreachType breach) {
-    if (breach == BreachType::TOO_LOW) {
-      cout << param << " too low!\n";
-    } else if (breach == BreachType::TOO_HIGH) {
-      cout << param << " too high!\n";
-    }
-  };
+  auto chargeRateBreach = getBreachType(chargeRate, 0, 0.8);
 
   printBreach("Temperature", tempBreach);
   printBreach("State of Charge", socBreach);
-  printBreach("Charge Rate", chargeRateBreach);
-
-  return tempBreach == BreachType::NORMAL &&
-         socBreach == BreachType::NORMAL &&
-         chargeRateBreach == BreachType::NORMAL;
-}
-
-int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true
+  print
